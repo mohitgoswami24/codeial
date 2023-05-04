e6d6cb75ba5e9199dae2258cec37228b828d0bd4
mohitgoswami24 <mohitgoswami24mg@gmail.com>
2023-05-04 22:03:28 +0530

    delete post only by Authorized user

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,4 +1,5 @@
 const Post = require('../models/post');
+const Comment = require('../models/comment');
 
 
 module.exports.create = async function(req,res){
@@ -9,3 +10,25 @@ module.exports.create = async function(req,res){
 
     return res.redirect('back');
 } 
+
+module.exports.destroy = async function(req, res){
+    
+    try {
+        const post = await Post.findById(req.params.id);
+
+        if (String(post.user) == req.user.id) {
+            await Post.findByIdAndDelete(req.params.id);
+            await Comment.deleteMany({ post: req.params.id });
+
+           
+            return res.redirect('back');
+        } else {
+           
+            return res.redirect('back');
+        }
+    } catch (error) {
+        console.log(error);
+        
+        return res.redirect('back');
+    }
+}
