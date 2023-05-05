1b633286d7de36621e5279eb08fc4e0a3d535920
mohitgoswami24 <mohitgoswami24mg@gmail.com>
2023-05-05 17:45:37 +0530

    delete comments only by authorized user

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -22,3 +22,27 @@ module.exports.create = async function(req,res){
 
     }
 } 
+
+
+module.exports.destroy = async function(req,res){
+    const comment = await Comment.findById(req.params.id);
+    const post = await Post.findById(comment.post);
+    try{
+        if(comment.user ==  req.user.id || post.user == req.user.id) {
+            let postID = comment.post;
+            await Comment.findByIdAndDelete(req.params.id);
+            await Post.findByIdAndUpdate(postID,{$pull:{comments:req.params.id}});
+
+    
+            return res.redirect('back');
+            }else{
+               
+                return res.redirect('back');
+        }
+    }catch(err){
+        console.log(err);
+       
+        return res.redirect('back');
+    }
+   
+}
