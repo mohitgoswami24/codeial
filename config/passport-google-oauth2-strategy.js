634d1768327ea3042717405f5cca797045a10eca
mohitgoswami24 <mohitgoswami24mg@gmail.com>
2023-05-19 10:37:24 +0530

    authentication using google oauth

---
+++ config/passport-google-oauth2-strategy.js
@@ -0,0 +1,37 @@
+const passport = require('passport');
+const googleStrategy = require('passport-google-oauth').OAuth2Strategy;
+const crypto = require('crypto');
+const User = require('../models/user');
+
+//tell passport to use a new strategy for google login
+passport.use(new googleStrategy({
+       clientID:"566885872689-cngkschmvqob2kaph6r5rk2rk3m4jdsj.apps.googleusercontent.com",
+       clientSecret:"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",
+       callbackURL:"http://localhost:9000/users/auth/google/callback"
+    },
+    async function(accessToken, refreshToken, profile, done){
+        //find a user
+        try{
+            const user = await User.findOne({email: profile.emails[0].value});
+            if(user){
+                //if found , set thi user as req.user
+                return done(null, user);
+            }else{
+                //if not found, create the user & set it as req.user
+                const Newuser = await User.create({
+                    name: profile.displayName,
+                    email: profile.emails[0].value,
+                    password: crypto.randomBytes(20).toString('hex')
+                });
+                return done(null, Newuser);
+            }
+        }catch(err){
+            console.log('Error in google strategy-passport',err);
+            return;
+        }
+        
+
+
+    }))
+
+module.exports = passport;
--- index.js
+++ index.js
@@ -9,6 +9,8 @@ const session = require('express-session');
 const passport = require('passport');
 const passportLocal = require('./config/passport-local-strategy');
 const passportJWT = require('./config/passport-jwt-strategy');
+const passportGoogle = require('./config/passport-google-oauth2-strategy');
+
 const MongoStore = require('connect-mongo');
 const sassMiddleware = require('node-sass-middleware');
 const flash = require('connect-flash');
@@ -18,6 +20,7 @@ const customMware = require('./config/middleware');
 
 
 app.use(sassMiddleware({
+
     src: './assets/scss',
     dest: './assets/css',
     debug: true,
